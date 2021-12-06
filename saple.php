60c366ec2880dc45129e4aecb15aa2058a00c119
RUUKU SEKIGUCHI <ruukure0827.1027@docomo.ne.jp>
2021-12-06 23:42:40 +0900

    参考サイト追加

--- saple.php
+++ saple.php
@@ -1,12 +1,12 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;dbname=dbname;charset=utf8','tablename','mypassword');
- 
+
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
         $st = $pdo->query("SELECT * FROM sensorvalues");
         echo json_encode($st->fetchAll(PDO::FETCH_ASSOC));
         break;
-         
+    
     case 'POST':
         $in = json_decode(file_get_contents('php://input'), true);
         if (!isset($in['id']))
@@ -14,9 +14,9 @@ switch ($_SERVER['REQUEST_METHOD']) {
                 $st = $pdo->prepare("INSERT INTO sensorvalues(datetime,temp,hum,press) 　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　VALUES(:datetime,:temp,:hum,:press)");
         }
         $st->execute($in);
-         
+        
         echo json_encode("normal end");
-         
+        
         break;
 }
 ?>
